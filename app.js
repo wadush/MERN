b000b5759997c222b8a5cfb506f05ef4cd341eb2
wadush <wadush@yahoo.com>
2021-05-13 12:56:02 +0200

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+
+async function start() {
+  try {
+      await mongoose.connect(config.get('mongoUri'), {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex:true
+
+      })
+      app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+       console.log('Server Eror', e.message)
+       process.exit(code:1)  
+    }
+}
+
+start()
